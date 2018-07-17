bec73ff7e4a2287a4a2f1d3af9c7e61e2096a0f3
Jared Wooten <pancakeparfait@gmail.com>
2018-07-16 22:47:36 -0400

    fixed failing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,31 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    this.fixture = TestBed.createComponent(AppComponent);
+    this.app = this.fixture.debugElement.componentInstance;
   }));
+  
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(this.app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+  
+  it(`should have as title 'Tour of Heroes'`, async(() => {
+    expect(this.app.title).toEqual('Tour of Heroes');
   }));
+
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-unit-tests-demo!');
+    this.fixture.detectChanges();
+    const compiled = this.fixture.debugElement.nativeElement;
+    
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   }));
 });
